87c8f8164248be2c2998b21a49425dc960d40b86
Kristiyan Tsankov <ktsankov@appolica.com>
2022-08-26 22:21:32 +0300

    add push notif android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:timeago/timeago.dart';
@@ -7,8 +8,13 @@ import 'app/app.dart';
 import 'app/locator.dart';
 import 'app/providers.dart';
 import 'services/localization_service.dart';
+import 'services/push_notifications_service.dart';
 import 'services/theme_service.dart';
 import 'utilitiies/bulgarian_messages.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
 
 Future<void> initApp() async {
   setup();
@@ -17,11 +23,16 @@ Future<void> initApp() async {
   await themeService.init();
   final localizationService = locator<LocalizationService>();
   await localizationService.init();
+  final pushService = locator<PushNotificationsService>();
+  await pushService.init();
 }
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(
+    _firebaseMessagingBackgroundHandler,
+  );
   await SystemChrome.setPreferredOrientations(
     [
       DeviceOrientation.portraitUp,
